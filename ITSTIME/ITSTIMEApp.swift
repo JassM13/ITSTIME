0aed3be43c254d7f060bc3b1966bd74184fee287
Jaspreet Malak <jass.malak13@gmail.com>
2022-05-13 18:17:27 -0700

    Initial Commit

---
+++ ITSTIME/ITSTIMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITSTIMEApp.swift
+//  ITSTIME
+//
+//  Created by Jaspreet Malak on 5/13/22.
+//
+
+import SwiftUI
+
+@main
+struct ITSTIMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
